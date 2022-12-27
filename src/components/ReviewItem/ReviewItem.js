1182bca083197d3f049045d9aa0c86b84d89d335
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-12-27 21:31:34 +0530

    v6 implement reviw items style fix

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import './ReviewItem.css'
 
 const ReviewItem = (props) => {
-    const {name,price,shipping,quantity} = props.product;
+    const {name,img,price,shipping,quantity} = props.product;
 
 
     return (
-        <div>
-            <h1>this is review item : {name}</h1>
-            <h1>price : ${price}</h1>
-            <p>shippin : {shipping}</p>
-            <p>quantity : {quantity}</p>
+        <div className='review-item'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="review-item-details-container">
+                <div className="review-item-details">
+                    <p className='name'title={name}>
+                      { name.length > 20 ? name.slice(0,20) +'...' : name }
+                    </p>
+                    <p>price : ${price}</p>
+                    <p>shipping : {shipping}</p>
+                    <p>quantity : {quantity}</p>
+                </div>
+                <div className="delete-container">
+                    <button>delete</button>
+                </div>
+
+            </div>
         </div>
     );
 };
